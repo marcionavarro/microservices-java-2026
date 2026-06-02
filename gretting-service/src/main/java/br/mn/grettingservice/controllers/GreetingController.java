fa41b3c2674c99d0a7180f4d250db4d33c5180d0
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-02 19:11:26 -0300

    #3 - Configurações Externalizadas

--- gretting-service/src/main/java/br/mn/grettingservice/controllers/GreetingController.java
+++ gretting-service/src/main/java/br/mn/grettingservice/controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package br.mn.grettingservice.controllers;
 
+import br.mn.grettingservice.configs.GreetingConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,16 +9,28 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    /*@Value("${greeting-service.greeting}")
+    private String greeting;
+
+    @Value("${greeting-service.default-name}")
+    private String defaultName;*/
+
+    private final GreetingConfig config;
+
+    public GreetingController(GreetingConfig config) {
+        this.config = config;
+    }
+
     @GetMapping({"", "/", "/{namePath}"})
     public ResponseEntity<String> getGreeting(
         @RequestParam(required = false) String name,
         @PathVariable(required = false) String namePath
     ) {
         if (name == null) {
-            name = namePath != null ? namePath : "World";
+            name = namePath != null ? namePath : config.getDefaultName();
         }
 
-        String retorno = String.format("%s %s!!!", "Hello", name);
+        String retorno = String.format("%s %s!!!", config.getDefaultName(), name);
         return ResponseEntity.ok(retorno);
     }
 
